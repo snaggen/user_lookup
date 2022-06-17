0ad7a62f04ffc302e8b980e2240adc65bea104ee
Mattias Eriksson <snaggen@mayam.com>
2022-06-17 14:19:51 +0200

    Avoid running codesample readein real passwd

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 //! from /etc/passwd and /etc/group. It uses tokio async and will cache the information for a
 //! duration specified by the user. If no caching is desired, a Duration of 0.0 can be used.
 //!
-//!```rust
+//!```rust,ignore
 //!use async_user_lookup::PasswdReader;
 //!use std::time::Duration;
 //!
@@ -19,7 +19,8 @@
 //!   let mut reader = PasswdReader::new(Duration::new(0,0));
 //!   let entries = reader.get_entries().await.unwrap();
 //!
-//!   println!("User with uid 1000 is: {}", reader.get_username_by_uid(1000).await.unwrap());
+//!   println!("User with uid 1000 is: {}",
+//!   reader.get_username_by_uid(1000).await.unwrap().unwrap());
 //!}
 //!
 //!```
